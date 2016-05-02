96059668058e9b993a603fd3e19106f1cfd29bb6
evil972 <4774552665@163.com>
2016-05-02 21:17:57 +0800

    Update git_update.sh

--- git_update.sh
+++ git_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cp -frp crysadm crysadm.old
 cd 2
-git clone https://github.com/seatom/crysadm.git
+git clone https://github.com/evil972/yjg.git
 rm crysadm/crysadm/config.py
 cp -frp crysadm/crysadm/* ../crysadm/
 ../run.sh
