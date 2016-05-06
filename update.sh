6b06a1b515ed8ba2a19344268ad6bd994fbfcc39
evil972 <4774552665@163.com>
2016-05-06 11:23:19 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 cp -frp crysadm crysadm.old
 cd tmp
 git clone https://github.com/evil972/yjg.git
-rm crysadm/crysadm/config.py
+rm yjg/tmp/crysadm/config.py
 cp -frp yjg/tmp/crysadm/* ../yjg/
 ../run.sh
 rm -rf *
