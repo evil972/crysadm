934d5c92b254ac85d64cf6bfe73a3f2c14ac73ec
evil972 <4774552665@163.com>
2016-05-11 13:48:30 +0800

    Delete update.sh

--- update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cp -frp crysadm crysadm.old
-cd tmp
-git clone https://github.com/evil972/yjg.git
-rm /yjg/tmp/yjg/crysadm/config.py
-cp -frp yjg/tmp/yjg/crysadm/* /yjg/
-/yjg/run.sh
-rm -rf *
